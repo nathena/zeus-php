834f7b360dfb73a9e85189b0d14a2e35c09ecca7
nathena <nathena@qq.com>
2016-09-27 13:30:36 +0800

    ide友好提示

--- application/controller/Index.php
+++ application/controller/Index.php
@@ -2,11 +2,15 @@
 namespace app\controller;
 
 use zeus\mvc\Controller;
+use zeus\db\DbManager;
+use zeus\db\driver\Pdo;
 
 class Index extends Controller
 {
 	public function index()
 	{
 		echo __CLASS__.' => '.__METHOD__;
+		
+		$pdo = DbManager::openSession();
 	}
 }
--- zeus/db/DbManager.php
+++ zeus/db/DbManager.php
@@ -3,46 +3,35 @@ namespace zeus\db;
 
 use zeus\db\driver\Pdo;
 use zeus\env\Env;
-use zeus\exception\UnSupportDbDriverException;
 
 class DbManager
 {
 	public static $driver_instances = [];
 	public static $xa_driver_instances = [];
 	
+	/**
+	 * 
+	 * @param string $alias
+	 * @return \zeus\db\driver\Pdo
+	 */
 	public static function openSession($alias='default')
 	{
 		if( !isset(self::$driver_instances[$alias]) )
 		{
-			$config = Env::database();
-			
-			$type = $config['type'];
-			if( 'pdo'!=$type )
-			{
-				throw new UnSupportDbDriverException($type);
-			}
-			
-			$driver = 'zeus\\db\\driver\\'.ucfirst($type);
-			
-			self::$driver_instances[$alias] = new $driver($config[$type]);
+			self::$driver_instances[$alias] = new Pdo(Env::database());
 		}
 		
 		return self::$driver_instances[$alias];
 	}
 	
+	/**
+	 * 
+	 * @param string $xid
+	 * @return \zeus\db\driver\Pdo
+	 */
 	public static function openXaSession($xid)
 	{
-		$config = Env::database();
-		
-		$type = $config['type'];
-		if( 'pdo'!=$type )
-		{
-			throw new UnSupportDbDriverException($type);
-		}
-		
-		$driver = 'zeus\\db\\driver\\'.ucfirst($type);
-		
-		$instance = new $driver($config[$type]);
+		$instance = new Pdo(Env::database());
 		$instance->xid($xid);
 		
 		self::$xa_driver_instances[] = $instance;
