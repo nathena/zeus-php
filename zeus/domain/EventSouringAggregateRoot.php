500f64d316bbf1f53c5b6a2d8c89d85d058a8c56
nathena@qq.com <nathena@qq.com>
2017-06-02 23:40:36 +0800

    change: EventSouring commit action.

--- zeus/domain/EventSouringAggregateRoot.php
+++ zeus/domain/EventSouringAggregateRoot.php
@@ -23,8 +23,19 @@ abstract class EventSouringAggregateRoot extends AggregateRoot
         return $domainEvents;
     }
 
+
+    public function commit(){
+	    //TODO
+        $events = $this->getEvents();
+        foreach ($events as $event){
+            $this->publishMessage($event);
+        }
+    }
+
     protected function raise(AbstractEvent $domainEvent)
     {
         $this->domainEvents[] = $domainEvent;
     }
+
+
 }
