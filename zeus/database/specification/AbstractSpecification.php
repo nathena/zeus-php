80da932fbd6f45670f8c598294e3720d89d5f27e
nathena <nathena@qq.com>
2017-06-07 11:49:51 +0800

    AbstractWhere

--- zeus/database/specification/AbstractSpecification.php
+++ zeus/database/specification/AbstractSpecification.php
@@ -11,9 +11,6 @@ namespace zeus\database\specification;
 
 abstract class AbstractSpecification
 {
-    protected $codition_data = [];
-    protected $having_data = [];
-
     protected $params = [];
     protected $sql = "";
 
@@ -36,59 +33,5 @@ abstract class AbstractSpecification
         return $this->dml;
     }
 
-    public function where($key, $value = NULL){
-        $this->_where($key,$value);
-        return $this;
-    }
 
-    public function or_where($key,$value=null){
-        $this->_where($key,$value," or ");
-        return $this;
-    }
-
-    public function where_in($key,$values,$not = false){
-        $this->_where_in($key,$values,$not);
-        return $this;
-    }
-
-    public function or_where_in($key,$values,$not = false){
-        $this->_where_in($key,$values,$not," or ");
-        return $this;
-    }
-
-    public function like($key,$match,$site="both",$not=false){
-        $this->_like($key,$match,$site,$not);
-        return $this;
-    }
-
-    public function or_like($key,$match,$site="both",$not=false){
-        $this->_like($key,$match,$site,$not," or ");
-        return $this;
-    }
-
-    public function having($key,$value){
-        $this->_having($key,$value);
-        return $this;
-    }
-
-    public function or_having($key,$value){
-        $this->_having($key,$value," or ");
-        return $this;
-    }
-
-    private function _where($key,$value=null,$type = 'AND '){
-        //TODO
-    }
-
-    private function _where_in($key,$values,$not= false, $type = " and "){
-        //TODO
-    }
-
-    private function _like($key,$match,$side="both",$not= false, $type = 'AND '){
-        //TODO
-    }
-
-    private function _having($key,$value,$type = " and "){
-        //TODO
-    }
 }
---
+++ zeus/database/specification/AbstractWhereSpecification.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nathena
+ * Date: 2017/6/7 0007
+ * Time: 11:47
+ */
+
+namespace zeus\database\specification;
+
+
+class AbstractWhereSpecification extends AbstractSpecification
+{
+    protected $codition_data = [];
+    protected $having_data = [];
+
+    public function __construct($dml)
+    {
+        parent::__construct($dml);
+    }
+
+    public function where($key, $value = NULL){
+        $this->_where($key,$value);
+        return $this;
+    }
+
+    public function or_where($key,$value=null){
+        $this->_where($key,$value," or ");
+        return $this;
+    }
+
+    public function where_in($key,$values,$not = false){
+        $this->_where_in($key,$values,$not);
+        return $this;
+    }
+
+    public function or_where_in($key,$values,$not = false){
+        $this->_where_in($key,$values,$not," or ");
+        return $this;
+    }
+
+    public function like($key,$match,$site="both",$not=false){
+        $this->_like($key,$match,$site,$not);
+        return $this;
+    }
+
+    public function or_like($key,$match,$site="both",$not=false){
+        $this->_like($key,$match,$site,$not," or ");
+        return $this;
+    }
+
+    public function having($key,$value){
+        $this->_having($key,$value);
+        return $this;
+    }
+
+    public function or_having($key,$value){
+        $this->_having($key,$value," or ");
+        return $this;
+    }
+
+    private function _where($key,$value=null,$type = 'AND '){
+        //TODO
+    }
+
+    private function _where_in($key,$values,$not= false, $type = " and "){
+        //TODO
+    }
+
+    private function _like($key,$match,$side="both",$not= false, $type = 'AND '){
+        //TODO
+    }
+
+    private function _having($key,$value,$type = " and "){
+        //TODO
+    }
+}
