79d5fa8eb9883d30fe0c1f305b6f985054580149
nathena <nathena@qq.com>
2017-06-07 11:46:12 +0800

    insertSpecification

--- zeus/database/specification/InsertSpecification.php
+++ zeus/database/specification/InsertSpecification.php
@@ -11,9 +11,28 @@ namespace zeus\database\specification;
 
 class InsertSpecification extends AbstractSpecification
 {
+    private $table;
+    private $insertSqlFormat = "INSERT INTO `%s` ( %s ) VALUES ( %s )";
 
-    public function __construct()
+    public function __construct($table,$params)
     {
         parent::__construct("insert");
+
+        $this->table = trim($table);
+        $this->prepare($params);
+    }
+
+    private function prepare($params){
+        $insert_key = $insert_value = $comma = "";
+        foreach($params as $field => $val )
+        {
+            $insert_key .= $comma . '`' .$field . '`';
+            $insert_value .= $comma.":".$field;
+            $comma = ",";
+
+            $this->params[":".$field] = $val;
+        }
+
+        $this->sql = sprintf($this->insertSqlFormat,$this->table,$insert_key,$insert_value);
     }
 }
