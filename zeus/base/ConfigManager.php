858cd30361a3760005e6ebd18e1eea621a958d41
nathena@qq.com <nathena@qq.com>
2017-06-07 21:56:27 +0800

    impove configmanager

--- zeus/base/ConfigManager.php
+++ zeus/base/ConfigManager.php
@@ -1,31 +1,32 @@
 <?php
+
 namespace zeus\sandbox;
 
-use zeus\base\exception\ConfigNotFoundException;
 use zeus\base\logger\Logger;
 
 class ConfigManager
 {
-	private static $config = [];
-	private static $inited = false;
+    private static $config = [];
+    private static $inited = false;
 
-	public static function config($key = '')
-	{
-	    if(empty($key)){
-	        return static::$config;
+    public static function config($key = '')
+    {
+        if (empty($key)) {
+            return static::$config;
         }
 
         $key = strtolower(trim($key));
-	    if(isset(static::$config[$key])){
+        if (isset(static::$config[$key])) {
             return static::$config[$key];
         }
         return static::$config;
-	}
+    }
 
-	public static function addRouter($config){
+    public static function addRouter($config)
+    {
         $routers = static::$config["router.rewrite"];
-        foreach($config as $r => $w){
-            if(isset($routers[$r])){
+        foreach ($config as $r => $w) {
+            if (isset($routers[$r])) {
                 throw new \RuntimeException(" {$r} url has already existed!");
             }
 
@@ -34,20 +35,21 @@ class ConfigManager
         static::$config["router.rewrite"] = $routers;
     }
 
-    public static function init(){
-        if( static::$inited ){
+    public static function init()
+    {
+        if (static::$inited) {
             return;
         }
 
-        $config = include_once ZEUS_PATH.DS."config.php";
-        static::$config = array_merge(static::$config,$config);
+        $config = include_once ZEUS_PATH . DS . "config.php";
+        static::$config = array_merge(static::$config, $config);
 
-        if(defined("APP_ENV_PATH") ){
+        if (defined("APP_ENV_PATH")) {
             $config_file = APP_ENV_PATH;
-            if( is_file($config_file)){
+            if (is_file($config_file)) {
                 $config = include_once $config_file;
-                static::$config = array_merge(static::$config,$config);
-            }else{
+                static::$config = array_merge(static::$config, $config);
+            } else {
                 Logger::warn("Application config : {$config_file} not found");
             }
         }
@@ -55,34 +57,4 @@ class ConfigManager
         static::$inited = true;
     }
 
-    private static function load($key)
-    {
-        $env_config_path = static::loadConfigFile($key);
-        if( is_file($env_config_path) )
-        {
-            return include_once $env_config_path;
-        }
-        return array();
-    }
-
-    private static function loadConfigFile($key){
-        $env_config_dir = defined("APP_ENV_DIR") ? APP_ENV_DIR : ZEUS_PATH;
-
-        $env_config_path = $env_config_dir.DS.$key.".php";
-        if(file_exists($env_config_path)){
-            return $env_config_path;
-        }
-
-        $env_config_path = $env_config_dir.DS."config.php";
-        if(file_exists($env_config_path)){
-            return $env_config_path;
-        }
-
-        $env_config_path = ZEUS_PATH.DS."config.php";
-        if(file_exists($env_config_path)){
-            return $env_config_path;
-        }
-
-        throw new ConfigNotFoundException("Config {$key} not found in {$env_config_path}");
-    }
 }
