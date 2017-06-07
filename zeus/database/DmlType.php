c67c0e7d6dca41024a242835b7c918c9f21bd8bc
nathena@qq.com <nathena@qq.com>
2017-06-07 21:43:50 +0800

    format

--- zeus/database/DmlType.php
+++ zeus/database/DmlType.php
@@ -10,10 +10,10 @@ namespace zeus\database;
 
 class DmlType
 {
-    const DML_SELECT_LIST = "select_list";
-    const DML_SELECT_ONE  = "select_one";
-    const DML_INSERT      = "INSERT";
-    const DML_INSERT_BATCH= "INSERT_BATCH";
-    const DML_UPDATE      = "UPDATE";
-    const DML_DELETE      = "DELETE";
+    const DML_SELECT_LIST   = "select_list";
+    const DML_SELECT_ONE    = "select_one";
+    const DML_INSERT        = "INSERT";
+    const DML_INSERT_BATCH  = "INSERT_BATCH";
+    const DML_UPDATE        = "UPDATE";
+    const DML_DELETE        = "DELETE";
 }
