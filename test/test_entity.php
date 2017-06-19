c1201059711a2f9552b83fc7fbd6388ae706b4d9
nathena <nathena@qq.com>
2017-06-19 11:43:48 +0800

    fix: test

--- test/test_entity.php
+++ test/test_entity.php
@@ -9,15 +9,15 @@ include_once "bootstrap.php";
 
 class User extends AbstractEntity
 {
-    public function __construct($data)
-    {
-        parent::__construct($data);
-    }
+    protected $schema = "t_user";
 }
 
-$user = new User(['id'=>222]);
+$user = new User();
+$user->setProperties(["a"=>'1','b'=>'2',"id"=>3]);
 $user->setData(["a"=>'a','b'=>'b',"id"=>2]);
 
 //var_dump($user);
 var_dump($user->getData());
-var_dump($user->getUpdatedData());
+var_dump($user->getProperties());
+var_dump($user->getSchema());
+var_dump($user->getId());
