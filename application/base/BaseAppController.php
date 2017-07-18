1896de5c097e42dd37e8f463194c78724002ff99
nathena <nathena@qq.com>
2017-07-18 14:39:51 +0800

    il8n支持

--- application/base/BaseAppController.php
+++ application/base/BaseAppController.php
@@ -9,9 +9,18 @@ namespace base;
 
 
 use zeus\mvc\Controller;
+use zeus\sandbox\ConfigManager;
 
 class BaseAppController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $config = ConfigManager::getInstance();
+        $config['il8n.config.path'] = __APPLICATION__.DS.'config'.DS.'chinese.ini';
+    }
+
     public function errorHandler(\Exception $e)
     {
         $this->modelMap['err_code'] = $e->getCode();
--- application/test/IndexController.php
+++ application/test/IndexController.php
@@ -8,14 +8,17 @@
 namespace test;
 
 
-use zeus\mvc\Controller;
+use base\BaseAppController;
+use zeus\utils\Il8n;
 
-class IndexController extends Controller
+class IndexController extends BaseAppController
 {
     public function index()
     {
         echo __NAMESPACE__,"<br />";
         echo __CLASS__,"<br />";
         echo $_SERVER['REQUEST_URI'],"<br />";
+
+        echo Il8n::get("您好，%s！","zzzzz","zzzzz111","zzzzz111","zzzzz111");
     }
 }
